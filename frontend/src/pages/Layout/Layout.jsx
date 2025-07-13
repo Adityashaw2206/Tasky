1f7a308012cdf1a04f936321782ff45bb9a7b5e6
Adityashaw2206 <shawaditya137@gmail.com>
2025-07-13 21:47:07 +0530

    commit

--- frontend/src/pages/Layout/Layout.jsx
+++ frontend/src/pages/Layout/Layout.jsx
@@ -1,21 +1,4 @@
-// // pages/Layout/index.jsx
-// import { Outlet } from 'react-router-dom';
-// import Sidebar from '../Sidebar/Sidebar.jsx';
-// const Layout = () => {
-//   return (
-//     <div className="flex h-screen">
-//       <Sidebar />
-//       <div className="flex-1 p-4 bg-gray-100 overflow-auto">
-//         <Outlet />
-//       </div>
-//     </div>
-//   );
-// };
 
-// export default Layout;
-
-
-// pages/Layout/index.jsx
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar.jsx';
 import { useState } from 'react';
